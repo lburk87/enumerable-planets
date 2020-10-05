e6028bcf0d114ac2fb58b632c13ea62f59886874
lburk87 <67616760+lburk87@users.noreply.github.com>
2020-10-05 18:05:11 -0500

    added enum Planets

--- src/main/java/org/launchcode/enumerableplanets/data/Planets.java
+++ src/main/java/org/launchcode/enumerableplanets/data/Planets.java
@@ -1,12 +1,25 @@
 package org.launchcode.enumerableplanets.data;
 
 public enum Planets {
-    MERCURY,
-    VENUS,
-    EARTH,
-    MARS,
-    JUPITER,
-    SATURN,
-    URANUS,
-    NEPTUNE
+
+    MERCURY("Mercury"),
+    VENUS("Venus"),
+    EARTH("Earth"),
+    MARS("Mars"),
+    JUPITER("Jupiter"),
+    SATURN("Saturn"),
+    URANUS("Uranus"),
+    NEPTUNE("Neptune");
+
+    private final String displayName;
+
+    Planets(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName(){
+        return displayName;
+    }
+
+
 }
